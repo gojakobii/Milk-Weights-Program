b3d95902630ddbd1410c0c9533942a3af5200c02
gojakobii <61474016+gojakobii@users.noreply.github.com>
2020-04-18 14:50:41 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,29 +1,19 @@
 package application;
 
-import java.awt.Checkbox;
 import java.util.List;
 
-import javax.swing.JTextArea;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -37,39 +27,37 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// save args example
+		//Save args example
 		args = this.getParameters().getRaw();
-		
-		final Button button = new Button ("Send");
-	    final Label notification = new Label ();
-	    
-		primaryStage.setTitle(APP_TITLE);
-		Scene scene = new Scene(new Group(), WINDOW_WIDTH, WINDOW_HEIGHT);
 
+		//Creating TextField for file input
 		TextField fileInput = new TextField();
 		fileInput.setPromptText("Input a valid text file (.csv)");
-		
-		ComboBox reportComboBox = new ComboBox();
+
+		//Creating ComboBox for report type selection
+		ComboBox<String> reportComboBox = new ComboBox<String>();
 		reportComboBox.getItems().addAll(
 				"Farm Report",
 				"Annual Report",
 				"Monthly Report",
 				"Data Range Report");
 		reportComboBox.setValue("Select a report");
-		
+
 		TextArea fileOutput = new TextArea();
 		fileOutput.setEditable(false);
 		fileOutput.setPromptText("Display for selected report type and statistics");
+
+		//Creating VBox layout manager for buttons
+		VBox vBox = new VBox();
 		
 		//Defining Buttons to operate on the file
-VBox vBox = new VBox();
-		
 		Button add = new Button("Add");
 		Button remove = new Button("Remove");
 		Button edit = new Button("Edit");
 		Button display = new Button("Display");
 		vBox.getChildren().addAll(add, remove, edit, display);
-		
+
+		//Adding to GridPane
 		GridPane grid = new GridPane();
 		grid.setVgap(4);
 		grid.setHgap(10);
@@ -81,12 +69,15 @@ VBox vBox = new VBox();
 		grid.add(fileOutput, 0, 1, 4, 1);
 		grid.add(vBox, 4, 1);
 
+		//Setting up stage and scene
+		Scene scene = new Scene(new Group(), WINDOW_WIDTH, WINDOW_HEIGHT);
 		Group root = (Group)scene.getRoot();
 		root.getChildren().add(grid);
+		primaryStage.setTitle(APP_TITLE);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}   
-	
+
 
 	/**
 	 * @param args
