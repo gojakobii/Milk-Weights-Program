b15d830ab9f115074bbd2b68dd7a49710242c498
sagrawal8 <61482309+sagrawal8@users.noreply.github.com>
2020-04-21 15:34:02 -0500

    Add files via upload

---
+++ Farm.java
@@ -0,0 +1,87 @@
+import java.util.List;
+import java.util.Map;
+
+public class Farm {
+
+   private int farmID;
+   Map<String, Integer> data;
+   
+   public Farm() {}
+   
+   /**
+    * adds a milk weight record to this farm's records.
+    */
+   private void add(String date, int milkWeight) {
+   
+   }
+   
+   /**
+    * adds a milk weight record to this farm's records.
+    */
+   private void edit(String date, int milkWeight) {
+   
+   }
+   
+   /**
+    * adds a milk weight record to this farm's records.
+    */
+   private void remove(String date) {
+   
+   }
+   
+   /**
+    * Minimum by month. 
+    * 
+    */
+   private List<String> minByMonth(String month){
+     //parameters of list could be changed
+     return null;
+   }
+   
+   /**
+    * Minimum by year. 
+    * 
+    */
+   private List<String> minByYear(String year){
+     //parameters of list could be changed
+     return null;
+   }
+   
+   /**
+    * Maximum by month. 
+    * 
+    */
+   private List<String> maxByMonth(String month){
+     //parameters of list could be changed
+     return null;
+   }
+   
+   /**
+    * Maximum by year. 
+    * 
+    */
+   private List<String> maxByYear(String year){
+     //parameters of list could be changed
+     return null;
+   }
+   
+   /**
+    * Avg by month. 
+    * 
+    */
+   private List<String> avgByMonth(String month){
+     //parameters of list could be changed
+     return null;
+   }
+   
+   /**
+    * Avg by year. 
+    * 
+    */
+   private List<String> avgByYear(String year){
+     //parameters of list could be changed
+     return null;
+   }
+   
+    
+}
