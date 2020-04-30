b6344df76f3c9f9713cc690b284e387d46a6e077
gojakobii <61474016+gojakobii@users.noreply.github.com>
2020-04-30 15:14:47 -0500

    Added more bad-data exception handling
    TableView outputs correct information per each report
    Instructions load
    Close button terminates the program
    TO-DO: Connect output to Main and also format GUI controls if we have time so things are hidden/shown on Scene rather than opening new Stage

--- Main.java
+++ Main.java
@@ -25,6 +25,10 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+/**
+ * Class that creates the MilkWeights GUI. This class contains 
+ * main and is not meant to be instantiated.
+ */
 public class Main extends Application {
 	private static final String[] MONTHS = {
 			"january", "february", "march", "april", "may", "june", "july", "august", "september", "november",
@@ -35,55 +39,98 @@ public class Main extends Application {
 	// NOTE: this.getParameters().getRaw() will get these also
 	private List<String> args;
 
-	private static final int WINDOW_WIDTH = 492;
-	private static final int WINDOW_HEIGHT = 439;
-	private static final String APP_TITLE = "Milk Weights Program";
-	private Farm farm;
-	private ArrayList<Details> displayFarms;
-	private TableView table = new TableView();
+	private static final int WINDOW_WIDTH = 513; //static width dimension for Scene
+	private static final int WINDOW_HEIGHT = 439; //static height dimension for Scene
+	private static final String APP_TITLE = "Milk Weights Program"; //static title for Scene
+	private Farm farm; //global Farm object to be operated on
+	private ArrayList<Details> displayFarms; //global displayFarms object to be operated on
+	private TableView table = new TableView(); //global TableView for inputting report data
 
+	/**
+	 * Inner class to format data with TableView
+	 *
+	 */
 	public class DetailsFormat {
 		private String farmID;
-		private int month;
+		private String month;
 		private int milkWeight;
-		private int totalWeight;
+		private String totalWeight;
 
-		private DetailsFormat(String farmID, int month, int milkWeight, int totalWeight) {
+		/**
+		 * Constructor for DetailsFormat object
+		 * @param farmID
+		 * @param month
+		 * @param milkWeight
+		 * @param totalWeight
+		 */
+		private DetailsFormat(String farmID, String month, int milkWeight, String totalWeight) {
 			this.farmID = farmID;
 			this.month = month;
 			this.milkWeight = milkWeight;
 			this.totalWeight = totalWeight;
 		}
 
+		/**
+		 * FarmID accessor method
+		 * @return farmID
+		 */
 		public String getFarmID() {
 			return farmID;
 		}
 
-		public Integer getMonth() {
+		/**
+		 * Month accessor method
+		 * @return month
+		 */
+		public String getMonth() {
 			return month;
 		}
 
+		/**
+		 * MilkWeight accessor method
+		 * @return milkWeight
+		 */
 		public Integer getMilkWeight() {
 			return milkWeight;
 		}
 
-		public Integer getTotalWeight() {
+		/**
+		 * TotalWeight accessor method
+		 * @return totalWeight
+		 */
+		public String getTotalWeight() {
 			return totalWeight;
 		}
 
+		/**
+		 * FarmID mutator method
+		 * @return
+		 */
 		public void setFarmID(String f) {
 			this.farmID = f;
 		}
 
-		public void setMonth(Integer m) {
+		/**
+		 * Month mutator method
+		 * @return
+		 */
+		public void setMonth(String m) {
 			this.month = m;
 		}
 
+		/**
+		 * MilkWeight mutator method
+		 * @return
+		 */
 		public void setMilkWeight(Integer mW) {
 			this.milkWeight = mW;
 		}
 
-		public void setTotalWeight(Integer tW) {
+		/**
+		 * TotalWeight mutator method
+		 * @return
+		 */
+		public void setTotalWeight(String tW) {
 			this.totalWeight = tW;
 		}
 	}
@@ -160,23 +207,70 @@ public class Main extends Application {
 	//		newWindow.show();
 	//	}
 
-	public void parse(String filename, Button submit) throws Exception {
-		System.out.println("load started.\n");
-
+	/**
+	 * Parses user supplied path name and loads all data into Farm object
+	 * 
+	 * @param filename
+	 * @param submit
+	 * @throws Exception - thrown if FileManager cannot parse the file due to invalid input
+	 */
+	private void parse(String pathName) throws Exception {
 		try {
 			FileManager fm = new FileManager();
-			this.farm = fm.loadFileOrFolder(filename);
-			System.out.println("load worked.\n");
-
+			this.farm = fm.loadFileOrFolder(pathName);
 		} catch (Exception e) {
-			throw new Exception();
+			throw new Exception(); //to be dealt with elsewhere in the form of an Error alert
 		}
 	}
+	
 
-	public void displayReport(String report, ArrayList<TextField> variables, TableView table) throws Exception{
+	/**
+	 * Given a user-inputted month (e.g. "5" or "may"), returns the correct month in the "MM" format (e.g. "05") that
+	 * can then be passed to the Farm class as a valid parameter.
+	 *
+	 * @param userInputMonth the user-inputted month string
+	 * @return the month in the MM format
+	 */
+	private String fixMonth(String userInputMonth) {
+		// Ignore whitespace at beginning and end of string, and ignore case
+		userInputMonth = userInputMonth.trim().toLowerCase();
+
+		// User inputted month as a single digit, e.g. "5", when it should be "05"
+		if (userInputMonth.length() == 1) return "0" + userInputMonth;
+
+		// User inputted month as the name of the month (possibly abbreviated)
+		if (userInputMonth.length() >= 3) {
+			for (int i = 0; i < MONTHS.length; i++) {
+				if (MONTHS[i].startsWith(userInputMonth)) {
+					// The user meant this month (MONTHS[i]) -- we should return the string value of (i+1)
+					String monthStr = String.valueOf(i + 1);
+					if (monthStr.length() == 1) monthStr = "0" + monthStr;
+					return monthStr;
+				}
+			}
+		}
+
+		// The user either already entered a month in the MM format, or put the month in an invalid format
+		return userInputMonth;
+	}
+
+	/**
+	 * Processes Farm file into a report which the user selects. The program then
+	 * adds that data into a TableView
+	 *  
+	 * @param report - type of report that user selects
+	 * @param variables - contains TextField submitted input
+	 * @param table
+	 * @throws Exception - thrown if entered data is invalid
+	 */
+	private void displayReport(String report, ArrayList<TextField> variables) throws Exception{
 		try {
-			DisplayStats dStats = new DisplayStats(farm);;
+			DisplayStats dStats = new DisplayStats(farm);
 			ArrayList<String> keys = new ArrayList<String>();
+			ArrayList<String> reportTotal;
+			String tableMonth = "";
+			boolean isFarmReport = false;
+			boolean check = false;
 
 			for (TextField t : variables) {
 				keys.add(t.getText()); //Gets the text from each TextField
@@ -186,44 +280,59 @@ public class Main extends Application {
 			if (report.equals("Farm Report")) {
 				String farmid = keys.get(0);
 				String year = keys.get(1);
-				//				System.out.println(farmid + year);
-				//				System.out.println("farm: " + farm.toString());
 
-				//				String [][] freport = dStats.farmReportResult(farmid, year);
-				//				for (int row = 0; row < freport.length; row++) {
-				//					for (int col = 0; col < freport[row].length; col++) {
-				//						System.out.print(freport[row][col]);
-				//					}
-				//					System.out.println();
-				//				}
-				System.out.println("ACCESSING FARM REPORT");
+				reportTotal = dStats.farmReportResult(farmid, year);
 				displayFarms = farm.farmReport(farmid, year);
-				System.out.println(displayFarms.size());
+				
+				//Exception checking
+				for (int i = 0; i < displayFarms.size(); i++) {
+					if (displayFarms.get(i).getFarmID().equals(farmid)) {
+						if (farm.getValues(new Date(1, 1, Integer.parseInt(year))).get(i) != null) { //placeholder date
+							check = true;
+							break;
+						}
+					}
+				}
+				if (!check) {
+					throw new Exception(); //invalid user data
+				}
+				
+				isFarmReport = true;
 			} else if (report.equals("Monthly Report")) {
 				String monthStr = fixMonth(keys.get(0));
 				String yearStr = keys.get(1);
-				//				System.out.println(month + year);
-				//				System.out.println("farm: " +farm.toString());
-				System.out.println("ACCESSING MONTH REPORT");
-
-				// TODO display a proper error message if an input was entered wrong
 				int year = Integer.parseInt(yearStr);
 				int month = Integer.parseInt(monthStr);
 
+				reportTotal = dStats.monthlyReportResult(month, year);
 				displayFarms = farm.monthlyReport(month, year);
-
-				//				String [][] freport = dStats.monthlyReportResult(month, year);
-				//				for (int row = 0; row < freport.length; row++) {
-				//					for (int col = 0; col < freport[row].length; col++) {
-				//						System.out.print(freport[row][col]);
-				//					}
-				//					System.out.println();
-				//				}
+				
+				for (int i = 0; i < displayFarms.size(); i++) {
+					if (farm.getValues(new Date(month, 1, year)).get(i) != null) { //placeholder date
+						check = true;
+						break;
+					}
+				}
+				if (!check) {
+					throw new Exception(); //invalid user data
+				}
+				
+				tableMonth = Integer.toString(month);
 			} else if (report.equals("Annual Report")) {
 				String year = keys.get(0);
 
-				System.out.println("ACCESSING ANNUAL REPORT");
+				reportTotal = dStats.annualReportResult(year);
 				displayFarms = farm.annualReport(year);
+				
+				for (int i = 0; i < displayFarms.size(); i++) {
+					if (farm.getValues(new Date(1, 1, Integer.parseInt(year))).get(i) != null) { //placeholder date
+						check = true;
+						break;
+					}
+				}
+				if (!check) {
+					throw new Exception(); //invalid user data
+				}
 			} else {
 				String yearStr = keys.get(0);
 				String startMonthStr = fixMonth(keys.get(1));
@@ -231,25 +340,49 @@ public class Main extends Application {
 				String endMonthStr = fixMonth(keys.get(3));
 				String endDayStr = keys.get(4);
 
-				// TODO display a proper error message if an input was entered wrong
 				int year = Integer.parseInt(yearStr);
 				int startMonth = Integer.parseInt(startMonthStr);
 				int startDay = Integer.parseInt(startDayStr);
 				int endMonth = Integer.parseInt(endMonthStr);
 				int endDay = Integer.parseInt(endDayStr);
 
-				System.out.println("ACCESSING DATE RANGE REPORT");
+				reportTotal = dStats.dateRangeResult(year, startMonth, startDay, endMonth, endDay);
 				displayFarms = farm.dateRange(year, startMonth, startDay, endMonth, endDay);
+				
+				for (int i = 0; i < displayFarms.size(); i++) {
+					if (farm.getValues(new Date(startMonth, startDay, year)).get(i) != null) {
+						check = true;
+						break;
+					}
+				}
+				if (!check || (startMonth > endMonth)) {
+					throw new Exception(); //invalid user data
+				}
+				check = false;
+				for (int i = 0; i < displayFarms.size(); i++) {
+					if (farm.getValues(new Date(endMonth, endDay, year)).get(i) != null) {
+						check = true;
+						break;
+					}
+				}
+				if (!check) {
+					throw new Exception(); //invalid user data
+				}
 			}
 
 			//Setting up TableView for report output
 			ObservableList<DetailsFormat> data = FXCollections.observableArrayList();
 
-			/**
-			 * MAKE IF STATEMENT SO NO MONTH APPEARS FOR OTHER REPORTS BESIDES FARM - ACCORDING TO SHASHANK
-			 */
-			for (Details d : displayFarms) {
-				data.add(new DetailsFormat(d.getFarmID(), d.getMonth(), d.getMilkWeight(), 0));
+			//Adding to TableView
+			for (int i = 0; i < displayFarms.size(); i++) {
+				Details d = displayFarms.get(i);
+				if (isFarmReport) { //output ascending month order for Farm Report
+					data.add(new DetailsFormat(d.getFarmID(), Integer.toString(d.getMonth()), 
+							d.getMilkWeight(), reportTotal.get(i)));
+				}
+				else {
+					data.add(new DetailsFormat(d.getFarmID(), tableMonth, d.getMilkWeight(), reportTotal.get(i)));
+				}
 			}
 			table.setItems(data);
 
@@ -265,7 +398,6 @@ public class Main extends Application {
 
 			table.getColumns().setAll(farmIDCol, monthCol, milkWeightCol, totalCol);
 		} catch (Exception e) {
-			e.printStackTrace();
 			throw new Exception();
 		}
 	}
@@ -277,21 +409,23 @@ public class Main extends Application {
 
 		//Creating TextField for file input
 		TextField fileInput = new TextField();
-		fileInput.setPromptText("Input the name of a .csv file or folder containing .csv");
+		fileInput.setMinWidth(170);
+		fileInput.setPromptText("Input valid file or folder path");
 
 		//Creating Button for instructions
 		Button instructions = new Button("Instructions");
 		instructions.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
-				Alert confirm = new Alert(AlertType.CONFIRMATION, "Please enter the folder path containing valid .csv document data"
-						+ " into the text field. Then select a report type:\n"
+				Alert confirm = new Alert(AlertType.CONFIRMATION, "Please enter the folder path or file path containing valid .csv document data"
+						+ " into the text field. Then select a report type:\n\n"
 						+ "\t1. Farm Report: Please enter a valid farm ID and year. \n"
 						+ "\t2. Annual Report: Please enter a valid year. \n"
 						+ "\t3. Monthly Report: Please enter a valid month and year. \n"
-						+ "\t4. Date Range Report: Please enter a valid year, starting day, starting month, ending month, and ending year. \n"
-						+ "(NOTE: All months and days less than 10, please enter with a 0 prefix (i.e. 01, 02, 03, etc))\n\n"
+						+ "\t4. Date Range Report: Please enter a valid year, starting \n\tday, starting "
+						+ "month, ending month, and ending year.\n\n"
 						+ "Once all fields have been filled in, click the 'Submit' button to display the corresponding report. \n\n"
-						+ "The 'Write' button allows the user to have their activity be output into a file. The 'Close' button allows the user"
+						+ "The 'Write' button allows the user to have their activity be output into a file. "
+						+ "The 'Close' button allows the user"
 						+ " to end the program. The 'Add', 'Edit', 'Remove', and 'Display' are included for display and do note perform "
 						+ "legitimate computations. Enjoy the program!");
 				confirm.show();
@@ -332,7 +466,7 @@ public class Main extends Application {
 			String select = reportComboBox.getSelectionModel().getSelectedItem();
 			ArrayList<TextField> variables = new ArrayList<TextField>();
 
-			//set up for various reports
+			//Set-up for various reports
 			if (select.equals("Farm Report")) {
 				gPane.add(new Label("Enter farm: "), 0, 0);
 				TextField tFarm = new TextField();
@@ -393,21 +527,21 @@ public class Main extends Application {
 			}
 
 			//Creating Button for submission of inserted text
-			//When clicked, file parses, and and info displays
+			//When clicked, file parses and info is displayed
 			Button submit = new Button("Submit!");
 			submit.setOnAction(new EventHandler<ActionEvent>() {
 				public void handle(ActionEvent event) {
 					newWindow.hide();
 
 					try {
-						parse(fileInput.getText(), submit);
-						displayReport(select, variables, table); //display that report
+						parse(fileInput.getText());
+						displayReport(select, variables); //display that report
 					} catch (Exception e) {
+						//Alerts the user that an Exception was thrown
 						Alert warning = new Alert(AlertType.ERROR, "Something went wrong! "
 								+ "Check that you've entered a valid file name and input valid information intended for "
 								+ "computation.");
 						warning.show();
-						e.printStackTrace();
 					}
 				}
 			});
@@ -422,7 +556,7 @@ public class Main extends Application {
 			newWindow.show();
 		});
 
-		//Defining Buttons to operate on the file
+		//Defining Add/Edit/Remove/Display Buttons to operate on the file
 		Button add = new Button("Add");
 		add.setMinWidth(78);
 		add.setAlignment(Pos.BASELINE_LEFT);
@@ -624,36 +758,6 @@ public class Main extends Application {
 		primaryStage.setTitle(APP_TITLE);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-	}
-
-	/**
-	 * Given a user-inputted month (e.g. "5" or "may"), returns the correct month in the "MM" format (e.g. "05") that
-	 * can then be passed to the Farm class as a valid parameter.
-	 *
-	 * @param userInputMonth the user-inputted month string
-	 * @return the month in the MM format
-	 */
-	private String fixMonth(String userInputMonth) {
-		// Ignore whitespace at beginning and end of string, and ignore case
-		userInputMonth = userInputMonth.trim().toLowerCase();
-
-		// User inputted month as a single digit, e.g. "5", when it should be "05"
-		if (userInputMonth.length() == 1) return "0" + userInputMonth;
-
-		// User inputted month as the name of the month (possibly abbreviated)
-		if (userInputMonth.length() >= 3) {
-			for (int i = 0; i < MONTHS.length; i++) {
-				if (MONTHS[i].startsWith(userInputMonth)) {
-					// The user meant this month (MONTHS[i]) -- we should return the string value of (i+1)
-					String monthStr = String.valueOf(i + 1);
-					if (monthStr.length() == 1) monthStr = "0" + monthStr;
-					return monthStr;
-				}
-			}
-		}
-
-		// The user either already entered a month in the MM format, or put the month in an invalid format
-		return userInputMonth;
 	}
 
 	/**
